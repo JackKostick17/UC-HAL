ae471be8bce9a976f0dd5a1959f35e8c475f7bd1
JackKostick17 <98375099+JackKostick17@users.noreply.github.com>
2022-09-16 11:43:20 -0600

    Add files via upload

---
+++ Mapping.R
@@ -0,0 +1,21 @@
+library('raster')
+library('ncdf4')
+library('sf')
+
+nc_file <- 'C:/Users/jackk/OneDrive/Documents/Thesis Work/Mackenzie Delta/ANHA4_mesh_mask.nc'
+#nc_ras <- brick(nc_file)
+
+
+nc_fo <- nc_open(nc_file)
+names(nc_fo$dim) #dimensions
+names(nc_fo$var) # variables
+
+
+lat <- ncvar_get(nc_fo, "nav_lat")
+lon <- ncvar_get(nc_fo, "nav_lon")
+
+coord <- data.frame(id=1:length(lon),lon=as.vector(lon), lat=as.vector(lat))
+
+dat_shp <- st_as_sf(coord, coords = c('lon','lat'), crs=4326)
+
+st_write(dat_shp,'C:/Users/jackk/OneDrive/Documents/Thesis Work/Mackenzie Delta/grid_cells.shp')
